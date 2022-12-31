dd050432f1411ac74a64ed71e439f4d0df1984d4
opeoluwa <opesiyanbola8991@gmail.com>
2022-12-31 14:37:03 +0100

    added image url type to category and product struct

--- models/category.model.go
+++ models/category.model.go
@@ -6,9 +6,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Category_Url struct {
+	Url string `json:"url" validate:"required"`
+	ID  string `json:"id" validate:"required"`
+}
+
 type Category struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	Name       *string            `json:"name" validate:"required,min=2,max=100"`
-	Created_at time.Time          `json:"created_at"`
-	Updated_at time.Time          `json:"updated_at"`
+	ID           primitive.ObjectID `bson:"_id"`
+	Name         *string            `json:"name" validate:"required,min=2,max=100"`
+	Category_Url Category_Url       `json:"category_url" validate:"required"`
+	Created_at   time.Time          `json:"created_at"`
+	Updated_at   time.Time          `json:"updated_at"`
 }
--- models/product.model.go
+++ models/product.model.go
@@ -6,6 +6,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Product_Url struct {
+	Url string `json:"url" validate:"required"`
+	ID  string `json:"id" validate:"required"`
+}
+
 type Product struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty"`
 	CategoryID  primitive.ObjectID `json:"category_id" validate:"required"`
@@ -13,6 +18,7 @@ type Product struct {
 	Description string             `json:"description"`
 	Price       float64            `json:"price" validate:"required"`
 	Quantity    int                `json:"quantity" validate:"required"`
+	Product_Url Product_Url        `json:"product_url" validate:"required"`
 	Created_at  time.Time          `json:"created_at"`
 	Updated_at  time.Time          `json:"updated_at"`
 }
